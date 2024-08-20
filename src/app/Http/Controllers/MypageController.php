8f5e84f2c9fdb8a54f83f43ed4db07a73debe00c
rino-0717 <rino.justinb@gmail.com>
2024-08-20 20:58:57 +0900

    mypageファイル修正

--- src/app/Http/Controllers/MypageController.php
+++ src/app/Http/Controllers/MypageController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Reservation;
+use App\Models\Like;
 
 class MyPageController extends Controller
 {
@@ -14,7 +17,8 @@ class MyPageController extends Controller
         $likes = $user->likes;
         return view('mypage', [
             'user' => $user,
-            // 他の必要なデータをここに追加
+            'reservations'=> $user,
+            'likes'=> $user,
         ]);
     }
 }
